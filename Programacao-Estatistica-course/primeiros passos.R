7a4ae9d19a901a26bcd3c7ba3210c9df2f172c26
Ellen Cristine <86074197+ellencrist@users.noreply.github.com>
2023-05-24 22:07:22 -0300

    Add files via upload

---
+++ Programacao-Estatistica-course/primeiros passos.R
@@ -0,0 +1,74 @@
+# Operações
+5/8
+
+5^9
+
+sqrt(4)
+
+3*7
+
+8-10
+
+5^(2+7)/3
+
+# Fatorial
+factorial(6)
+
+# Arrendodamento
+round(23.3902,3)
+
+
+# igualdade - comparação
+3==3
+
+#negacao = diferente(!=)
+3!=4
+4!=4
+
+#maior menor (><)
+3>9
+6<8
+
+# operador 'e' (&)
+3==3 & 4==2
+2==2 & 5!=2
+
+#operador ou (|)
+8!=6 | 7==7
+3!=3 | 4==7
+
+
+#imprimir texto
+print('Hello world')
+
+
+#condicional 
+if(4==3) "certo" else "errado"
+if(4!=4) "verdadeiro "else "falso"
+
+
+if(3==3 & 7==8) 3:2 else 5-2
+
+
+if(5==3) 6^2
+if(5!=5) 6^2
+
+# contagem (for) laço de repeticao
+
+for (i in 1:10){
+  print(i)
+}
+
+
+for (i in 1:10) {print(i); if (i==5) break}
+
+print(1:10)
+
+#instalar pacotes
+install.packages("dplyr")
+
+#utilizar pacote
+library(dplyr)
+
+
+
